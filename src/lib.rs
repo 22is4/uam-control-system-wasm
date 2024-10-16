32ba079f3dd933b0b4893d550475391e1ba861fc
JiSeong Lee <cocoquiet@knu.ac.kr>
2024-10-16 22:28:36 +0900

    Feat: Add UAM struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+pub struct UAM {
+    id: u32,
+    time_boot_ms: u32,
+    lat: f64,
+    lon: f64,
+    alt: f64,
+    relative_alt: f32,
+    vx: f32,
+    vy: f32,
+    vz: f32,
+    hdg: f32,
+}
+
+#[wasm_bindgen]
+pub struct UAMs {
+    uams: Vec<UAM>,
 }
