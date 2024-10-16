24cc6be4ccc3fb65f4e750ca99117bb70f351d04
JiSeong Lee <cocoquiet@knu.ac.kr>
2024-10-16 22:36:28 +0900

    Feat: Change attribute data type & Add UAM impl

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,67 @@ pub struct UAM {
     lat: f64,
     lon: f64,
     alt: f64,
-    relative_alt: f32,
-    vx: f32,
-    vy: f32,
-    vz: f32,
-    hdg: f32,
+    relative_alt: f64,
+    vx: f64,
+    vy: f64,
+    vz: f64,
+    hdg: f64,
 }
 
 #[wasm_bindgen]
 pub struct UAMs {
     uams: Vec<UAM>,
+}
+
+#[wasm_bindgen]
+impl UAM {
+    pub fn new(
+        id: u32,
+        time_boot_ms: u32,
+        lat: f64,
+        lon: f64,
+        alt: f64,
+        relative_alt: f64,
+        vx: f64,
+        vy: f64,
+        vz: f64,
+        hdg: f64,
+    ) -> UAM {
+        UAM {
+            id,
+            time_boot_ms,
+            lat,
+            lon,
+            alt,
+            relative_alt,
+            vx,
+            vy,
+            vz,
+            hdg,
+        }
+    }
+
+    pub fn predict_collision(&self, other: &UAM) -> Option<f64> {
+        let dx = self.lat - other.lat;
+        let dy = self.lon - other.lon;
+        let dz = self.alt - other.alt;
+        let dvx = self.vx - other.vx;
+        let dvy = self.vy - other.vy;
+        let dvz = self.vz - other.vz;
+        let a = dvx * dvx + dvy * dvy + dvz * dvz; // dot product of dv and dv
+        let b = 2.0 * (dx * dvx + dy * dvy + dz * dvz); // dot product of d and dv
+        let c = dx * dx + dy * dy + dz * dz; // dot product of d and d
+        let d = b * b - 4.0 * a * c; // discriminant
+
+        if d < 0.0 {
+            return None;
+        }
+
+        let t = (-b - d.sqrt()) / (2.0 * a);
+        if t < 0.0 {
+            return None;
+        }
+
+        Some(t)
+    }
 }
